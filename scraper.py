3248583bc29ba4d440bf1b6c3161f5d93bc925d7
Bunescu Vasile <bunescu.vasile@gmail.com>
2021-10-07 20:55:28 +0300

    3.0 working project, need add parsig slots

--- scraper.py
+++ scraper.py
@@ -5,12 +5,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import time
-import pyautogui
 import json
 
 PATH = "../chromedriver 2"
-# cui = "38781286"
+# cui = "38781286, 1819608, 20649658, 15227070, 4774845"
 
 options = Options()
 options.page_load_strategy = "eager"
@@ -21,23 +19,21 @@ def initialization(input_list):
     results = []
     for cui in input_list:
         driver.get("https://www.romanian-companies.eu/search.asp")
-        getting_cui()
+        getting_cui(cui)
         item = paersin_one()
         results.append(item)
     save_to_file(results)
 
-def getting_cui():
+def getting_cui(cui):
     try:
-        for x in input_list:
-            ss = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "searchfor")))
-            # time.sleep(5)
-            driver.implicitly_wait(10)
-            ss.send_keys(x + Keys.ENTER)
-            driver.implicitly_wait(10)
-            WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, 'input-group-btn'))).click()
-            driver.implicitly_wait(10)
-            driver.find_element(By.CLASS_NAME, 'clickable-row').click()
-            driver.switch_to_window(driver.window_handles[1])
+        ss = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "searchfor")))
+        driver.implicitly_wait(10)
+        ss.send_keys(cui + Keys.ENTER)
+        driver.implicitly_wait(10)
+        WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, 'input-group-btn'))).click()
+        driver.implicitly_wait(10)
+        driver.find_element(By.CLASS_NAME, 'clickable-row').click()
+        driver.switch_to_window(driver.window_handles[1])
     except Exception as e:
         print(e)
 
@@ -51,8 +47,6 @@ def paersin_one():
     name = fields[1] 
     fiscal = fields[2]
     other = fields[3]
-   
-    print("ww38781286")
 
     return  {
         'name' : name.text,
